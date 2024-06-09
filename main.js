c90e3bdf145b179b9601a393797afd14ff9e452d
Atrayee-Das <atrayeekol@gmail.comgit config --global user.name Atrayee-Dasgit config user.e-mailatrayeekol@gmail.com git config user.name Atrayee-Dasgit configuser.namegit config --list --show-origin$ git config --list --show-origin git config --global user.email atrayeekol@gmail.comgit config --global user.name Atrayee-Dasgit config user.namegit commit -m 'First Commit'git commit -m 'First Commit'git config --list --show-origin$ git config --global user.email atrayeekol@gmail.com>
2024-06-09 13:18:25 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,55 @@
+// old way to use fetch()
+//
+// fetch("https://dog.ceo/api/breeds/list/all").then(function(resp
+// onse){
+// return response.json();
+// }).then(function(data){
+// console.log(data);
+// });
+// New way of using fetch()
+// we will use asynchronous functions
+async function start(){
+// await makes a function wait for a promise.
+const response = await
+fetch("https://dog.ceo/api/breeds/list/all");
+const data = await response.json();
+// console.log(data.message);
+createBreedList(data.message);
+}
+start();
+// following function will take the dog data object
+// And create a dropdown list.
+function createBreedList(breedList){
+const breedElem = document.getElementById("breed");
+breedElem.innerHTML = `<select
+onchange="loadByBreed(this.value)">
+<option>Choose a dog breed</option>
+${Object.keys(breedList).map(function(breed){
+return `<option>${breed}</option>`;
+}).join('')}
+</select>`;
+}
+// following function will load the images as we choose a breed
+// option from dropdown
+async function loadByBreed(breed){
+if(breed != 'Choose a dog breed'){
+// alert(breed);
+const response = await
+fetch(`https://dog.ceo/api/breed/${breed}/images`);
+const data = await response.json();
+console.log(data.message);
+showImages(data.message);
+}
+}
+function showImages(images){
+if(images.length > 0){
+const slideshowElement =
+document.getElementById('slideshow');
+slideshowElement.innerHTML = `<ul class="dogimglist">
+${Object.keys(images).map(function(image){
+return `<li><img
+src="${images[image]}"/></li>`;
+}).join('')}
+</ul>`;
+}
+}
